cb2e542953506b75e249c5c23f87d5282e4edb1e
vinpink <mysunshine2126@gmail.com>
2026-06-05 16:47:47 +0800

    feat: redis文档存储

---
+++ day06/02-redis-document-storage.py
@@ -0,0 +1,21 @@
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, StorageContext
+from llama_index.storage.docstore.redis import RedisDocumentStore
+
+
+from load_llm_models import get_llm
+
+# 初始化模型
+llm, embed_model = get_llm()
+
+# 加载文档
+documents = SimpleDirectoryReader(input_files=["../data/西游记第一回.txt"]).load_data()
+
+# 初始化RedisDocumentStore对象
+doc_store = RedisDocumentStore.from_host_and_port(host="localhost", port=6379, namespace="redis_document_store")
+
+storage_context = StorageContext.from_defaults(docstore=doc_store)
+
+# 第二种方式: 创建VectorStoreIndex才能进行存储
+# 创建上下文存储容器VectorStoreIndex
+VectorStoreIndex.from_documents(documents, storage_context=storage_context)
+print(storage_context.docstore.docs)
