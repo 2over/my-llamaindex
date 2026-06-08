9e78ed29d5bf47fea1f456226fb133be65e083c7
vinpink <mysunshine2126@gmail.com>
2026-06-08 14:48:31 +0800

    feat: 节点后置处理器

--- day06/07-custom-engine.py
+++ day06/07-custom-engine.py
@@ -25,51 +25,6 @@ documents = SimpleDirectoryReader(input_files=["../data/小说.txt"]).load_data(
 splitter = SentenceSplitter(chunk_size=512)
 nodes = splitter.get_nodes_from_documents(documents)
 
-# class CustomRetriever(BaseRetriever):
-#     """执行语义搜索和简单关键字搜索的自定义检索器。"""
-#     # 手动创建一个混合检索
-#
-#     def __init__(
-#             self,
-#             vector_retriever: VectorIndexRetriever,
-#             keyword_retriever: KeywordTableSimpleRetriever,
-#             mode: str = "AND",
-#     ) -> None:
-#         """Init params."""
-#
-#         self._vector_retriever = vector_retriever
-#         self._keyword_retriever = keyword_retriever
-#         if mode not in ("AND", "OR"):
-#             raise ValueError("Invalid mode.")
-#         self._mode = mode
-#         super().__init__()
-#
-#     def _retrieve(self, query_bundle: QueryBundle) -> List[NodeWithScore]:
-#         """Retrieve nodes given query."""
-#
-#         # 分别从两个引擎中检索相关的节点（卡片）
-#         vector_nodes = self._vector_retriever.retrieve(query_bundle)
-#         keyword_nodes = self._keyword_retriever.retrieve(query_bundle)
-#
-#         # 提取两组结果的 Node ID，方便做数学集合运算  {"1", "2", "3", "4", "5", "6"}
-#         vector_ids = {n.node.node_id for n in vector_nodes}
-#         keyword_ids = {n.node.node_id for n in keyword_nodes}
-#
-#         # 创建一个字典，把 ID 和节点对象关联起来，方便通过 ID 找回对象；去重后的上下文
-#         combined_dict = {n.node.node_id: n for n in vector_nodes}  # 将vector_nodes中的数据变成{"1": node, "2": node}
-#         # 将keyword_nodes中的数据变成{"1": node, "2": node}
-#         combined_dict.update({n.node.node_id: n for n in keyword_nodes})
-#
-#         if self._mode == "AND":
-#             # 获取两个检索器交集(重叠)的数据
-#             retrieve_ids = vector_ids.intersection(keyword_ids)
-#         else:
-#             # 获取两个检索器并集（不重叠）的数据
-#             retrieve_ids = vector_ids.union(keyword_ids)
-#
-#         # 根据最终确定的 ID 列表，从字典中取出完整的节点对象
-#         retrieve_nodes = [combined_dict[rid] for rid in retrieve_ids]
-#         return retrieve_nodes
 class CustomRetriever(BaseRetriever):
     """执行语义搜索和简单关键字搜索的自定义检索器"""
     # 手动创建一个混合检索
